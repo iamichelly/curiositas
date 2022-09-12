c72eda76b79085ca67f70a5511c3a67cf24afdc5
apfdamascena <apfd@cin.ufpe.br>
2022-09-12 16:02:26 -0300

    feature(home): base project

---
+++ curiositas/curiositas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  curiositas
+//
+//  Created by alexdamascena on 12/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
