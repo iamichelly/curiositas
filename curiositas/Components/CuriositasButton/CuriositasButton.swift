dddf69d8185674b0e33878015ea470dfaf06baa5
apfdamascena <apfd@cin.ufpe.br>
2022-09-13 21:35:03 -0300

    feature(home): home done

--- curiositas/Components/CuriositasButton/CuriositasButton.swift
+++ curiositas/Components/CuriositasButton/CuriositasButton.swift
@@ -7,31 +7,45 @@
 
 import UIKit
 
-struct CuriositasButtonViewModel {
-    let title: String
-}
-
-
 class CuriositasButton: UIButton {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupButton()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupButton(){
-        let configuration = UIButton.Configuration.filled()
-        self.configuration = configuration
     }
 }
 
 extension CuriositasButton {
     
     func configure(with model: CuriositasButtonViewModel){
-        self.setTitle(model.title, for: .normal)
+        
+        var configuration = model.type == .filled ?
+        UIButton.Configuration.filled() : UIButton.Configuration.plain()
+        configuration.titleAlignment = .center
+        configuration.cornerStyle = .capsule
+        
+        let colorText = model.type == .filled ? UIColor(named: ColorConstants.shared.PRIMARY_COLOR)! : .white
+                
+        let mySelectedAttributedTitle = NSAttributedString(string: model.title.rawValue,
+                                                           attributes: [
+                                                            NSAttributedString.Key.foregroundColor: colorText,
+                                                            NSAttributedString.Key.font: UIFont.rounded(ofSize: 20, weight: .bold)])
+        self.setAttributedTitle(mySelectedAttributedTitle, for: .normal)
+        
+        if model.type == .bordered {
+            self.layer.borderWidth = 1
+            self.layer.borderColor = UIColor.white.cgColor
+            self.layer.cornerRadius = 26
+        }
+        
+        if model.type == .filled {
+            self.tintColor = .white
+        }
+        
+        self.configuration = configuration
     }
 }
+
---
+++ curiositas/Components/CuriositasButton/CuriositasTitleType.swift
@@ -0,0 +1,17 @@
+//
+//  CuriositasTitleType.swift
+//  curiositas
+//
+//  Created by alexdamascena on 13/09/22.
+//
+
+import Foundation
+
+
+enum CuriositasTitleType: String {
+    
+    case play = "JOGAR"
+    case instructions = "INSTRUÇÕES"
+    case next = "PASSAR"
+    case ok = "OK"
+}
