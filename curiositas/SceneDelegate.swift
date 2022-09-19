8651778054eadcac2bc7a4b59201425b657ddadc
Sofia Melo <sofianovica@hotmail.com>
2022-09-19 15:31:54 -0300

    feature(popup): add right screen

--- curiositas/SceneDelegate.swift
+++ curiositas/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             viewControllerToShow =  HomeViewController()
         } else {
 //            viewControllerToShow = OnboardingViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
-            viewControllerToShow = PlayViewController()
+            viewControllerToShow = HomeViewController()
         }
         
         let window = UIWindow(windowScene: windowScene)
